02c2f314e64ea5e19a1381ec0853eccb1237aa3b
luke lincoln <luke@canva.com>
2020-12-30 00:08:21 +1100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,8 @@
 import * as LReact from './l_react';
 import { GameComponent } from './pacman/index';
-const Element = LReact.Element;
 
-class Thao extends LReact.Component {
-    render() {
-        return Element('div', {}, [
-            'Thao',
-        ]);
-    }
-}
-
-function Luke({}: {}) {
-
-    const [lukeColor, setLukeColor] = LReact.useState('red');
-    const [lincolnColor, setLincolnColor] = LReact.useState('orange');
-
-    return Element('div', {}, [
-        Element('div', { style: { color: lukeColor } }, ['Luke']),
-        Element('div', { style: { color: lincolnColor } }, ['Lincoln']),
-        Element('button', {
-            onClick: () => {
-                const newColor = lukeColor === 'red' ? 'blue' : 'red';
-                setLukeColor(newColor);
-            },
-        }, ['change luke']),
-        Element('button', {
-            onClick: () => {
-                const newColor = lincolnColor === 'orange' ? 'purple' : 'orange';
-                setLincolnColor(newColor);
-            },
-        }, ['change lincoln']),
-    ]);
-}
-
-
-function App({}: {}) {
-    return Element('div', {}, [
-        Element(GameComponent),
-        Element(Luke),
-        Element(Thao),
-    ]);
+function App() {
+    return LReact.Element(GameComponent);
 };
 
-LReact.RenderDom(Element(App), document.body);
+LReact.RenderDom(LReact.Element(App), document.body);
